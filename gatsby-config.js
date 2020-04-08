5b70686c0b220dcb7fb45797ef06d789caf399d9
Luccas Avila <hey@luccasavila.dev>
2020-04-07 22:50:39 -0300

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteMetadata: {
+    title: "Luccas Avila",
+    position: "Desenvolvedor Full-Stack",
+    description: `Portfólio e Blog de um grande amante de tecnologias`,
+    authorDescription:
+      "Desenvolvedor Full-Stack, das mais diversas tecnologias",
+    author: `@luccasdev`,
+    siteUrl: "https://luccasavila.dev",
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Luccas Avila`,
+        short_name: `LAvila`,
+        start_url: `/`,
+        background_color: `#221d23`,
+        theme_color: `#221d23`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
